5dcec0b52da2675a28be0754c13d6607dd129e82
John Erik Wittern <erikwittern@gmail.com>
2017-07-23 15:53:30 -0400

    revised localStorage names to ensure backwards-compatability

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,15 @@
 
 /* global localStorage */
 
+/**
+ * Names to use in local storage.
+ * These are horrible, but chosen to guarantee backwards-compatability.
+ */
+const LEVELS = 'gravity-gun'
+const MUTED = 'gravity-gun-mute'
+
 const storeLevelResult = (level, collectedGoodies) => {
-  let stats = JSON.parse(localStorage.getItem('gravityquest'))
+  let stats = JSON.parse(localStorage.getItem(LEVELS))
   if (stats) {
     if (typeof stats[level] === 'undefined' ||
       collectedGoodies > stats[level]) {
@@ -12,11 +19,11 @@ const storeLevelResult = (level, collectedGoodies) => {
     stats = []
     stats[level] = collectedGoodies
   }
-  localStorage.setItem('gravityquest', JSON.stringify(stats))
+  localStorage.setItem(LEVELS, JSON.stringify(stats))
 }
 
 const loadLevelStats = () => {
-  let stats = JSON.parse(localStorage.getItem('gravityquest'))
+  let stats = JSON.parse(localStorage.getItem(LEVELS))
   if (stats && Array.isArray(stats)) {
     return stats
   }
@@ -24,11 +31,11 @@ const loadLevelStats = () => {
 }
 
 const storeMuted = (muted) => {
-  localStorage.setItem('gravityquest-muted', JSON.stringify(muted))
+  localStorage.setItem(MUTED, JSON.stringify(muted))
 }
 
 const loadMuted = () => {
-  let muted = JSON.parse(localStorage.getItem('gravityquest-muted'))
+  let muted = JSON.parse(localStorage.getItem(MUTED))
   if (typeof mute !== 'boolean') {
     return muted
   }
